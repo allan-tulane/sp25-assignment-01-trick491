2e3cf6ba009f37dcbd7918b1229b9a2aa857028d
trick491 <143121688+trick491@users.noreply.github.com>
2025-02-17 13:37:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,25 @@ See assignment-01.pdf for details.
 # no imports needed.
 
 def foo(x):
-    ### TODO
+    if(x <= 1):
+        return x
+    else:
+        return((foo(x-1)) + (foo(x-2)))
     pass
 
 def longest_run(mylist, key):
     ### TODO
+    current = 0 
+    longest = [0]
+    for num in mylist:
+        if num==key:
+            current+=1
+        else:
+            longest.append(current)
+            current=0
+    longest.append(current)
+    return(max(longest))
+
     pass
 
 
@@ -38,6 +52,43 @@ def to_value(v):
         
 def longest_run_recursive(mylist, key):
     ### TODO
+    #base cases
+    if not mylist:
+        return Result(0,0,0, False)
+
+    #length =1
+    if len(mylist) == 1:
+        if mylist[0] == key:
+            count =1
+        else:
+            count = 0
+        return Result(count, count, count, mylist[0] == key)
+
+    mid = len(mylist)//2
+    left = longest_run_recursive(mylist[:mid],key)
+    right = longest_run_recursive(mylist[mid:],key)
+
+    #merging
+    # is the entire range the key
+    is_entire_range = left.is_entire_range and right.is_entire_range
+    #left side
+    if not left.is_entire_range:
+        left_size = left.left_size
+    else:
+        left_size = left.left_size +right.left_size
+
+    #right side
+    if not right.is_entire_range:
+        right_size = right.right_size
+    else:
+        right_size = left.right_size +right.right_size
+
+    #longest_size
+    longest_size = max(left.longest_size, right.longest_size, left.right_size + right.left_size)
+
+
+    return Result(left_size, right_size, longest_size, is_entire_range)
+
     pass
 
 
